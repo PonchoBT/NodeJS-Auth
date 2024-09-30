66d66d9bfd6d77792d747022a15fc1904bb45cc2
PonchoBT <ponchobt.dev@gmail.com>
2024-09-29 22:47:34 -0600

    Corrected server

--- config/database.js
+++ config/database.js
@@ -1,36 +1,18 @@
-// const mongoose = require("mongoose");
-// require('dotenv').config();
-
-// const MongoDB = process.env.MONGO_URL;
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(MongoDB);  // Elimina las opciones de configuración obsoletas
-//         console.log("MongoDB is connected");
-//     } catch (err) {
-//         console.error(`Error connecting to MongoDB: ${err.message}`);
-//         process.exit(1);  // Detener la aplicación si la conexión falla
-//     }
-// };
-
-// module.exports = connectDB;
-
-
 const mongoose = require("mongoose");
 require('dotenv').config();
+
+// Desactivar strictQuery si no lo necesitas
+mongoose.set('strictQuery', false);
 
 const MongoDB = process.env.MONGO_URL;  // Asegúrate de que esté bien escrito
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MongoDB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(MongoDB);
         console.log("MongoDB is connected");
     } catch (err) {
         console.error(`Error connecting to MongoDB: ${err.message}`);
-        process.exit(1); // Detener la aplicación si falla la conexión
+        process.exit(1);  // Detener la aplicación si la conexión falla
     }
 };
 
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
 const connectDB = require("./config/database");
-
 connectDB();
+const app = express()
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server is running successfully on port ${PORT}`)
-);
+const server = app.listen(PORT, () => console.log(`Server is running successfully on port ${PORT}`));
 
 process.on("unhandledRejection", (err) => {
   console.log(`error occured: ${err.message}`);
